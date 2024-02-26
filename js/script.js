4ba657339c0404f5392edf78ec712269950350fe
SouBeatrizKaroline <soubeatrizkaroline@gmail.com>
2024-02-26 17:11:18 -0300

    Structure Initial

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = new Swiper(".carousel", {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
