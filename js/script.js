e65a7b61da9796a22087cd7ebaba1603ecbecb6d
SouBeatrizKaroline <soubeatrizkaroline@gmail.com>
2024-03-10 10:22:18 -0300

    Margem da pagina de artigos e responsividade do header

--- js/script.js
+++ js/script.js
@@ -19,12 +19,3 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.mobile-menu-icon');
-    const navMenu = document.querySelector('nav ul');
-
-    menuIcon.addEventListener('click', function () {
-        navMenu.classList.toggle('active');
-    });
-});
